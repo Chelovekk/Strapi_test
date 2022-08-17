a7a94a53d4c82269f2808a1fa80430dc5ad2ffcb
vadym <vadym.toporovskyi@sapient.pro>
2022-08-17 17:59:37 +0300

    new structure

--- src/api/test-industry/controllers/test-industry.ts
+++
@@ -1,7 +0,0 @@
-/**
- *  test-industry controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::test-industry.test-industry');
--- src/api/test-industry/routes/test-industry.ts
+++
@@ -1,7 +0,0 @@
-/**
- * test-industry router.
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::test-industry.test-industry');
